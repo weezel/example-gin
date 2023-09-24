f11f4fb27b07bb3e8544098ef5e1e753da0a8008
Ville Valkonen <weezel@users.noreply.github.com>
2023-09-24 11:52:01 +0300

    Rename initializer.go to controller.go.
    Also implement per test case dependant method mocker so each test can
    implement their own method calls on the fly.
    
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

---
+++ cmd/webserver/routes/user/controller.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"context"
+	"weezel/example-gin/pkg/generated/sqlc"
+)
+
+type HandlerController struct {
+	querier sqlc.Querier
+}
+
+func NewHandlerController(db sqlc.Querier) *HandlerController {
+	return &HandlerController{querier: db}
+}
+
+// MockHandlerController introduces method calls that can be implemented on test case basis
+type MockHandlerController struct {
+	mAdduser    func(context.Context, sqlc.AddUserParams) (int32, error)
+	mDeleteUser func(context.Context, string) (*sqlc.HomepageSchemaUser, error)
+	mGetUser    func(context.Context, string) (*sqlc.HomepageSchemaUser, error)
+	mListUsers  func(context.Context) ([]*sqlc.HomepageSchemaUser, error)
+}
+
+func (h MockHandlerController) AddUser(ctx context.Context, arg sqlc.AddUserParams) (int32, error) {
+	return h.mAdduser(ctx, arg)
+}
+
+func (h MockHandlerController) DeleteUser(ctx context.Context, name string) (*sqlc.HomepageSchemaUser, error) {
+	return h.mDeleteUser(ctx, name)
+}
+
+func (h MockHandlerController) GetUser(ctx context.Context, name string) (*sqlc.HomepageSchemaUser, error) {
+	return h.mGetUser(ctx, name)
+}
+
+func (h MockHandlerController) ListUsers(ctx context.Context) ([]*sqlc.HomepageSchemaUser, error) {
+	return h.mListUsers(ctx)
+}
