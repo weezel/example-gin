b1b71ccd1ae227fef4c881437ab4f3b9e3d872b4
Ville Valkonen <weezel@users.noreply.github.com>
2023-09-24 22:19:00 +0300

    Save some bytes by rearranging fields.
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- cmd/webserver/routes/user/params.go
+++ cmd/webserver/routes/user/params.go
@@ -1,7 +1,7 @@
 package user
 
 type User struct {
+	Name string `form:"name" binding:"required" json:"name"`
 	ID   int32  `form:"id" validate:"gte=0" json:"id"`
 	Age  int32  `form:"age" validate:"gte=0,lte=130" json:"age"`
-	Name string `form:"name" binding:"required" json:"name"`
 }
