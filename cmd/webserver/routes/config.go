d256c6830f77d5206f531e47d4cf7d5d3406ebe4
Ville Valkonen <weezel@users.noreply.github.com>
2023-12-12 08:22:20 +0200

    Use options pattern to implement the HTTP server.
    Also substitute obsoleted jaeger package with otlptracehttp paclage.
    
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- cmd/webserver/routes/config.go
+++ cmd/webserver/routes/config.go
@@ -1,21 +1,18 @@
 package routes
 
 import (
-	"weezel/example-gin/cmd/webserver/routes/general"
 	"weezel/example-gin/cmd/webserver/routes/user"
 	"weezel/example-gin/pkg/generated/sqlc"
-
-	"github.com/gin-gonic/gin"
+	"weezel/example-gin/pkg/httpserver"
 )
 
 // Add routes to our web server
-func AddRoutes(r *gin.Engine, queries *sqlc.Queries) {
+func AddRoutes(httpServer *httpserver.HTTPServer, queries *sqlc.Queries) {
 	ctrl := user.NewHandlerController(queries) // TODO
-	r.GET("/health", general.HealthCheckHandler)
 
 	// User related functionality
-	r.GET("/user/", ctrl.IndexHandler)
-	r.GET("/user/:name", ctrl.GetHandler)
-	r.POST("/user", ctrl.PostHandler)
-	r.DELETE("/user", ctrl.DeleteHandler)
+	httpServer.GET("/user/", ctrl.IndexHandler)
+	httpServer.GET("/user/:name", ctrl.GetHandler)
+	httpServer.POST("/user", ctrl.PostHandler)
+	httpServer.DELETE("/user", ctrl.DeleteHandler)
 }
