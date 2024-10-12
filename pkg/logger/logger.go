f1bd5c7db9fd810f15fd8ee92a4da31f52528dd9
Ville Valkonen <weezel@users.noreply.github.com>
2024-10-12 23:11:11 +0300

    Make caller's file path relative to .git dir.
    If .git directory is found, show caller's file path relative to that,
    otherwise use the absolute path.
    
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -16,6 +17,57 @@ var (
 	Logger zerolog.Logger
 	uniqID string
 )
+
+//nolint:gochecknoinits // init() is allowed here to avoid needless calls in each package. Import should be eonugh.
+func init() {
+	var logLevel zerolog.Level
+	if strings.ToLower(os.Getenv("DEBUG")) == "true" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		logLevel = zerolog.DebugLevel
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		logLevel = zerolog.InfoLevel
+	}
+
+	uniqID = randID()
+
+	startDir, err := os.Getwd()
+	if err != nil {
+		Logger.Fatal().Err(err).Msg("Failed to get working directory")
+	}
+
+	gitRoot, err := findGitRootDir(startDir)
+	if err == nil {
+		// Found a git dir
+		zerolog.CallerMarshalFunc = func(_ uintptr, file string, line int) string {
+			// Make caller's filename path relative to the git root
+			relPath, err := filepath.Rel(gitRoot, file)
+			if err == nil {
+				return fmt.Sprintf("%s:%d", relPath, line)
+			}
+			// Fallback to full path if relative conversion fails
+			return fmt.Sprintf("%s:%d", file, line)
+		}
+	}
+
+	zerolog.TimeFieldFormat = time.RFC3339
+	zerolog.DurationFieldUnit = time.Second
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack //nolint:reassign // From their own example snippet
+
+	Logger = zerolog.New(os.Stdout).
+		Level(logLevel).
+		With().
+		Timestamp().
+		Str("uniq_id", uniqID).
+		Caller().
+		Logger()
+
+	if isInteractive() {
+		Logger = Logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+
+	Logger.Debug().Msgf("Starting logger on level %s", Logger.GetLevel())
+}
 
 func randID() string {
 	data := make([]byte, 8)
@@ -36,33 +88,22 @@ func isInteractive() bool {
 	return (fileInfo.Mode() & os.ModeCharDevice) != 0
 }
 
-//nolint:gochecknoinits // init() is allowed here to avoid needless calls in each package. Import should be eonugh.
-func init() {
-	var logLevel zerolog.Level
-	if strings.ToLower(os.Getenv("DEBUG")) == "true" {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		logLevel = zerolog.DebugLevel
-	} else {
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
-		logLevel = zerolog.InfoLevel
+// findGitRootDir finds the directory where the .git directory is located
+func findGitRootDir(startDir string) (string, error) {
+	currentDir := startDir
+	for {
+		// Check if the ".git" directory exists in the current directory
+		gitPath := filepath.Join(currentDir, ".git")
+		if stat, err := os.Stat(gitPath); err == nil && stat.IsDir() {
+			return currentDir, nil
+		}
+
+		// Move one level up
+		parentDir := filepath.Dir(currentDir)
+		if parentDir == currentDir {
+			// Reached the root of the filesystem
+			return "", fmt.Errorf("could not find .git directory")
+		}
+		currentDir = parentDir
 	}
-
-	uniqID = randID()
-
-	zerolog.TimeFieldFormat = time.RFC3339
-	zerolog.DurationFieldUnit = time.Second
-	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack //nolint:reassign // From their own example snippet
-	Logger = zerolog.New(os.Stdout).
-		Level(logLevel).
-		With().
-		Timestamp().
-		Str("uniq_id", uniqID).
-		Caller().
-		Logger()
-
-	if isInteractive() {
-		Logger = Logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	}
-
-	Logger.Debug().Msgf("Starting logger on level %s", Logger.GetLevel())
 }
